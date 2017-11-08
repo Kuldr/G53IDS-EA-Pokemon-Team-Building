c5b18d66f30432711b137f2161c8c9b7d430d9c5
Kuldr <kuldrgames@gmail.com>
2017-11-08 17:23:38 +0000

    Moved all methods into a seperate class

--- Code/GeneticAlgorithm.py
+++ Code/GeneticAlgorithm.py
@@ -6,14 +6,14 @@ from xSquared import xSquared
 
 #Set up
 random.seed()
-xS = xSquared()
+problem = xSquared()
 
 #Initialise and evaluate population
 population = []
 fitness = []
 for i in range(0, constants.POPULATION_SIZE):
-    population.append(xS.initialiseIndividual())
-    fitness.append(population[i][0]**2 + population[i][1]**2)
+    population.append(problem.initialiseIndividual())
+    fitness.append(problem.objectiveValue(population[i]))
 
 #Termination Criteria loop, runs for a set number of generations
 for x in range(0, constants.NUMBER_OF_GENERATIONS):
@@ -24,41 +24,30 @@ for x in range(0, constants.NUMBER_OF_GENERATIONS):
     for i in range(0, constants.POPULATION_SIZE/2):
         #Selection Criteria for parents
         #Currently just random for simplictity
-        indexParent1 = random.randrange(constants.POPULATION_SIZE)
-        indexParent2 = random.randrange(constants.POPULATION_SIZE)
+        indexParent1 = problem.selection(constants.POPULATION_SIZE)
+        indexParent2 = problem.selection(constants.POPULATION_SIZE)
 
         #Apply Crossover to generate offspring
-        children.append([population[indexParent1][0], population[indexParent2][1]])
+        children.append(problem.crossover(population[indexParent1], population[indexParent2]))
 
         #Apply Mutation
-        if( random.random() <= constants.MUTATION_RATE ):
-            children[i][random.randrange(2)] *= random.uniform(-3.0, 3.0)
+        children[i] = problem.mutation(children[i], constants.MUTATION_RATE)
 
         #Validation
-        if( children[i][0] > 10 ):
-            children[i][0] = 10
-        if( children[i][1] > 10 ):
-            children[i][1] = 10
-        if( children[i][0] < -10 ):
-            children[i][0] = -10
-        if( children[i][1] < -10 ):
-            children[i][1] = -10
+        children[i] = problem.validation(children[i])
 
         #Evaluate fitness
-        childrenFitness.append(children[i][0]**2 + children[i][1]**2)
+        childrenFitness.append(problem.objectiveValue(children[i]))
 
     #Population Replacement
     for i in range(0, len(children)):
-        #Random Replacement but only if child is better than previous member
-        indexToChange = random.randrange(constants.POPULATION_SIZE)
-        if( fitness[indexToChange] > childrenFitness[i] ):
-            population[indexToChange] = children[i]
-            fitness[indexToChange] = childrenFitness[i]
+        population, fitness = problem.populationReplacement(population, fitness, children[i], childrenFitness[i], constants.POPULATION_SIZE)
+
 
 #print out the best value
 best = fitness[0]
 for i in range(1, constants.POPULATION_SIZE):
     if( fitness[i] < best ):
         best = fitness[i]
-if( best != 0 ):
+if( best >= 1 ):
     print(best)
--- Code/xSquared.py
+++ Code/xSquared.py
@@ -1,4 +1,39 @@
+import random
+
 class xSquared:
 
     def initialiseIndividual(self):
-        return [1,2]#[random.randrange(-5, 5),random.randrange(-5, 5)]
+        return [random.randrange(-5, 5),random.randrange(-5, 5)]
+
+    def objectiveValue(self, individual):
+        return individual[0]**2 + individual[1]**2
+
+    def selection(self, populationSize):
+        return random.randrange(populationSize)
+
+    def crossover(self, parent1, parent2):
+        return [parent1[0], parent2[1]]
+
+    def mutation(self, child, mutationRate):
+        if( random.random() <= mutationRate ):
+            child[random.randrange(2)] *= random.uniform(-3.0, 3.0)
+        return child
+
+    def validation(self, child):
+        if( child[0] > 10 ):
+            child[0] = 10
+        if( child[1] > 10 ):
+            child[1] = 10
+        if( child[0] < -10 ):
+            child[0] = -10
+        if( child[1] < -10 ):
+            child[1] = -10
+        return child
+
+    def populationReplacement(self, population, fitness, child, childFitness, populationSize):
+        #Random Replacement but only if child is better than previous member
+        indexToChange = random.randrange(populationSize)
+        if( fitness[indexToChange] > childFitness ):
+            population[indexToChange] = child
+            fitness[indexToChange] = childFitness
+        return population, fitness
