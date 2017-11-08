183f537f6452bb3fadbd9b30c005b99201983cd1
Kuldr <kuldrgames@gmail.com>
2017-11-08 12:31:04 +0000

    Created the base GA structure

---
+++ Code/GeneticAlgorithm.py
@@ -0,0 +1,17 @@
+#IMPORTS
+import constants
+
+#Initialise and evaluate population
+
+#Termination Criteria loop, runs for a set number of generations
+for x in range(0, constants.NUMBER_OF_GENERATIONS):
+    #Main GA loop
+    for i in range(0, constants.POPULATION_SIZE/2):
+        print(i)
+        #Selection Criteria
+
+        #Apply Crossover to generate offspring
+
+        #Apply Mutation
+
+    #Population Replacement
