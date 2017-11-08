e0e1fc3088852b77d40b86a51adb9edc6b139281
Kuldr <kuldrgames@gmail.com>
2017-11-08 13:21:20 +0000

    added population generation

--- Code/GeneticAlgorithm.py
+++ Code/GeneticAlgorithm.py
@@ -3,13 +3,22 @@ import random
 
 import constants
 
+#Set up
+random.seed()
+
 #Initialise and evaluate population
+population = []
+fitness = []
+for i in range(0, constants.POPULATION_SIZE):
+    population.append([random.randrange(-5, 5),random.randrange(-5, 5)])
+    fitness.append(population[i][0]**2 + population[i][1]**2)
+print(population)
+print(fitness)
 
 #Termination Criteria loop, runs for a set number of generations
 for x in range(0, constants.NUMBER_OF_GENERATIONS):
     #Main GA loop
     for i in range(0, constants.POPULATION_SIZE/2):
-        print(random.randrange(constants.POPULATION_SIZE))
         #Selection Criteria for parents
         #Currently just random for simplictity
         indexParent1 = random.randrange(constants.POPULATION_SIZE)
