406e99e566d7fc2ca221a91a4753bbe7c749c94c
Kuldr <kuldrgames@gmail.com>
2018-01-31 19:17:03 +0000

    More representation as ids

--- Code/pokemon.py
+++ Code/pokemon.py
@@ -8,6 +8,12 @@ class GENDER(Enum):
 
 class pokemon:
 
+    #TODO: Hardcoded values deal with it
+    genderDictionary = {GENDER.MALE: "(M) ",
+                        GENDER.FEMALE: "(F) ",
+                        GENDER.GENDERLESS: ""}
+    natureDictionary = {}
+
     def __init__(self):
         self.level = 51
 
@@ -17,14 +23,14 @@ class pokemon:
 
     #Class Variables
     formID = 25 #Pikachu #Stored as the form id from which other infomation can be derived
-    gender = GENDER.GENDERLESS #Stored as Enum
-    item = 213 #Light Ball #Stored as item id
+    gender = GENDER.FEMALE #Stored as Enum
+    itemID = 213 #Light Ball #Stored as item id
     ability = 3 #Lightning Rod #Stored as the ability slot of that pokemon, to get further information on the ability would need to get all of the pokemons abilities and then iterate over them until the slot number matches
     level = 51 #Stored as an int
     shiny = True #Do I need #TODO
     happiness = 249 #Stored as an int
 
-    nature = "Mild" #Convert to enum #TODO
+    natureID = 8 #Mild #Stored as pokebase nature id
 
     #EVs stored as ints for that value
     evHP = 120
@@ -42,28 +48,16 @@ class pokemon:
     ivSpD = 25
     ivSpe = 28
 
-    #TODO MOVES
-    move1 = "Discharge" #Convert to ids
-    move2 = "Extreme Speed"
-    move3 = "Fake Out"
-    move4 = "Hidden Power [Fire]"
+    #MOVES
+    #TODO: Hidden Power it won't calculate the type :/
+    move1 = 435 #"Discharge" #Convert to ids (None for no move)
+    move2 = 245 #"Extreme Speed"
+    move3 = 252 #"Fake Out"
+    move4 = 237 #"Hidden Power [Fire]"
 
     #Methods
     def __str__(self):
         return self.showdownFormat
-
-    def strFormName(self):
-        return str(pb.pokemon(self.formID))
-
-    def strGender(self):
-        dictionary = {GENDER.MALE: "(M) ", GENDER.FEMALE: "(F) ", GENDER.GENDERLESS: ""}
-        return dictionary[self.gender]
-
-    def strItem(self):
-        return pb.item(self.item).name
-
-    def strAbility(self, ability):
-        return ability.name
 
     def getAbility(self):
         abilities = pb.pokemon(self.formID).abilities
@@ -71,64 +65,98 @@ class pokemon:
             if( abilities[i].slot == self.ability):
                 return abilities[i].ability
 
-    def strLevel(self):
-        return str(self.level)
-
-    def strShiny(self):
+    def showdownFormat(self):
+        s = "" #Start a blank string and then add each part to it individually
+        s = s + str(pb.pokemon(self.formID)) + " "
+        s = s + self.genderDictionary[self.gender]
+        s = s + "@ " + pb.item(self.itemID).name + "\n" #TODO
+        s = s + "Ability: " + self.getAbility().name + "\n"
+        s = s + "Level: " + str(self.level) + "\n"
         if( self.shiny == True ):
-            return "Shiny: Yes\n"
-        else:
-            return ""
-
-    def strHappiness(self):
-        return str(self.happiness)
-
-    def strEvs(self):
-        s = "EVs: "
+            s = s + "Shiny: Yes\n"
+        s = s + "Happiness: " + str(self.happiness) + "\n"
+        s = s + "EVs: "
         s = s + str(self.evHP) + " HP / "
         s = s + str(self.evAtk) + " Atk / "
         s = s + str(self.evDef) + " Def / "
         s = s + str(self.evSpA) + " SpA / "
         s = s + str(self.evSpD) + " SpD / "
         s = s + str(self.evSpe) + " Spe\n"
-        return s
-
-    def strNature(self):
-        return self.nature + " Nature\n"
-
-    def strIVs(self):
-        s = "IVs: "
+        s = s + pb.nature(self.natureID).name.title() + " Nature\n"
+        #TODO title to make nature capitalised so showdown accepts it
+        s = s + "IVs: "
         s = s + str(self.ivHP) + " HP / "
         s = s + str(self.ivAtk) + " Atk / "
         s = s + str(self.ivDef) + " Def / "
         s = s + str(self.ivSpA) + " SpA / "
         s = s + str(self.ivSpD) + " SpD / "
         s = s + str(self.ivSpe) + " Spe\n"
-        return s
-
-    def strMoves(self):
-        #TODO: WHAT IF NOT 4 MOVES
-        s = "- " + self.move1 + "\n"
-        s = s + "- " + self.move2 + "\n"
-        s = s + "- " + self.move3 + "\n"
-        s = s + "- " + self.move4
-        return s
-
-    def showdownFormat(self):
-        #TODO DO I MAKE THIS ONLY CALL ALL THE METHODS AND ADD NO EXTRA
-        s = "" #Start a blank string and then add each part to it individually
-        s = s + self.strFormName() + " "
-        s = s + self.strGender()
-        s = s + "@ " + self.strItem() + "\n"
-        s = s + "Ability: " + self.strAbility(self.getAbility()) + "\n"
-        s = s + "Level: " + self.strLevel() + "\n"
-        s = s + self.strShiny()
-        s = s + "Happiness: " + self.strHappiness() + "\n"
-        s = s + self.strEvs()
-        s = s + self.strNature()
-        s = s + self.strIVs()
-        s = s + self.strMoves()
+        #MOVES
+        if( self.move1 != None ):
+            s = s + "- " + pb.move(self.move1).name + "\n"
+        if( self.move2 != None ):
+            s = s + "- " + pb.move(self.move2).name + "\n"
+        if( self.move3 != None ):
+            s = s + "- " + pb.move(self.move3).name + "\n"
+        if( self.move4 != None ):
+            s = s + "- " + pb.move(self.move4).name + "\n"
         return s
 
 p = pokemon()
 print(p.showdownFormat())
+
+# Previously created strings for the showdownFormat
+#     def strFormName(self):
+#         return str(pb.pokemon(self.formID)) + " "
+#
+#     def strGender(self):
+#         dictionary = {GENDER.MALE: "(M) ", GENDER.FEMALE: "(F) ", GENDER.GENDERLESS: ""}
+#         return dictionary[self.gender]
+#
+#     def strItem(self):
+#         return pb.item(self.itemID).name
+#
+#     def strAbility(self, ability):
+#         return ability.name
+#
+#     def strLevel(self):
+#         return str(self.level)
+#
+#     def strShiny(self):
+#         if( self.shiny == True ):
+#             return "Shiny: Yes\n"
+#         else:
+#             return ""
+#
+#     def strHappiness(self):
+#         return str(self.happiness)
+#
+#     def strEvs(self):
+#         s = "EVs: "
+#         s = s + str(self.evHP) + " HP / "
+#         s = s + str(self.evAtk) + " Atk / "
+#         s = s + str(self.evDef) + " Def / "
+#         s = s + str(self.evSpA) + " SpA / "
+#         s = s + str(self.evSpD) + " SpD / "
+#         s = s + str(self.evSpe) + " Spe\n"
+#         return s
+#
+#     def strNature(self):
+#         return self.nature + " Nature\n"
+#
+#     def strIVs(self):
+#         s = "IVs: "
+#         s = s + str(self.ivHP) + " HP / "
+#         s = s + str(self.ivAtk) + " Atk / "
+#         s = s + str(self.ivDef) + " Def / "
+#         s = s + str(self.ivSpA) + " SpA / "
+#         s = s + str(self.ivSpD) + " SpD / "
+#         s = s + str(self.ivSpe) + " Spe\n"
+#         return s
+#
+#     def strMoves(self):
+#         s = "- " + self.move1 + "\n"
+#         s = s + "- " + self.move2 + "\n"
+#         s = s + "- " + self.move3 + "\n"
+#         s = s + "- " + self.move4
+#         return s
