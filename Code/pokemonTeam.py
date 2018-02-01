950e63ce44ffdca35bf7ef6da95ae5599a7b3390
Kuldr <kuldrgames@gmail.com>
2018-02-01 12:30:44 +0000

    Made Pokemon Team Instance Variables

--- Code/pokemonTeam.py
+++
@@ -1,9 +0,0 @@
-class pokemonTeam:
-
-    #Class Variables
-    pokemon1 = None
-    pokemon2 = None
-    pokemon3 = None
-    pokemon4 = None
-    pokemon5 = None
-    pokemon6 = None
---
+++ Code/pokemonTeamIndividual.py
@@ -0,0 +1,10 @@
+class pokemonTeamIndividual:
+
+    def __init__(self, pokemon1, pokemon2, pokemon3,
+                    pokemon4, pokemon5, pokemon6):
+        self.pokemon1 = pokemon1
+        self.pokemon2 = pokemon2
+        self.pokemon3 = pokemon3
+        self.pokemon4 = pokemon4
+        self.pokemon5 = pokemon5
+        self.pokemon6 = pokemon6
