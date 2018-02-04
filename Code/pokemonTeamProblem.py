09fff57ad3982a900bbe7d1b8fa8a0cdafd12f60
Kuldr <kuldrgames@gmail.com>
2018-02-04 22:41:36 +0000

    Began notes on mutation

--- Code/pokemonTeamProblem.py
+++ Code/pokemonTeamProblem.py
@@ -163,17 +163,102 @@ class pokemonTeamProblem:
 
         return pokemonTeamIndividual(p1, p2, p3, p4, p5, p6)
 
-    def mutation(self, child, mutationRate):
-        #Randomly mutate the individual based upon the mutation rate, then randomly mutate either variable by multipling by a real number in the range (-3, 3)
-
-        return None
-        # if( random.random() <= mutationRate ):
-        #     if( random.randrange(2) == 0 ):
-        #         child.x *= random.uniform(-3.0, 3.0)
-        #     else:
-        #         child.y *= random.uniform(-3.0, 3.0)
-        # return child
-
+    # def mutation(self, child, mutationRate):
+    #     #Randomly mutate the individual based upon the mutation rate, then randomly mutate either variable by multipling by a real number in the range (-3, 3)
+    #
+    #     NUMBER_OF_MUTATIONS = 1 #TODO: FIND A WAY TO AUTOMAGICALLY GENERATE THIS
+    #     # MAYBE A LIST OF MUTATION FUNCTIONS THAT YOU RANDOMLY SELECT FROM
+    #     # CAN YOU FIND OUT THE NUMBER OF METHODS IN ANOTHER CLASS
+    #
+    #     if( random.random() <= mutationRate ):
+    #         # Select a random team member to mutate
+    #         pokemon = [child.pokemon1, child.pokemon2, child.pokemon3,
+    #                    child.pokemon4, child.pokemon5, child.pokemon6]
+    #         randPokeIndex = random.randrange(6)
+    #         toMutate = pokemon[randPokeIndex]
+    #         r = random.randrange(NUMBER_OF_MUTATIONS)
+    #         if( r == 0 ):
+    #             # Ruin and recreate a random pokemon
+    #             toMutate = self.initialisePokemonIndividual()
+    #         elif( r == 1 ):
+    #             # Randomly change the pokemon gender
+    #
+    #         elif( r == 2 ):
+    #             # Randomly change the pokemon ability
+    #
+    #         elif( r == 3 ):
+    #             # Randomly change the pokemon level
+    #             toMutate.level = random.randrange(constants.MIN_LEVEL, constants.MAX_LEVEL+1)
+    #             # TODO: INCREASE/DECREASE LEVEL AMOUNT
+    #
+    #         elif( r == 4 ):
+    #             # Randomly change the pokemon shiny
+    #             toMutate.shiny = random.choice([True, False])
+    #
+    #         elif( r == 5 ):
+    #             # Randomly change the pokemon happiness
+    #             toMutate.happiness = random.randrange(constants.MIN_HAPPINESS, constants.MAX_HAPPINESS+1)
+    #             #TODO: INCREASE / DECREASE HAPPINESS AMOUNT
+    #
+    #         elif( r == 6 ):
+    #             # Randomly change the pokemon nature
+    #             toMutate.natureID = random.randrange(constants.MIN_NATUREID, constants.MAX_NATUREID+1)
+    #
+    #         elif( r == 7 ):
+    #             # Randomly change nature to another nature with the same +ive
+    #
+    #         elif( r == 8 ):
+    #             # Randomly change nature to another nature with the same -ive
+    #
+    #         elif( r == 9 ):
+    #             # Ruin and Recreate the whole EVs
+    #
+    #         elif( r == 10 ):
+    #             # Swap 2 sets of EVs
+    #
+    #         elif( r == 11 ):
+    #             # Decrease 1 EV and increase another by a % ammount
+    #
+    #         elif( r == 12 ):
+    #             # Decrease 1 EV and increase another by a integer ammount
+    #
+    #         elif( r == 13 ):
+    #             # Ruin and Recreate the IVs
+    #
+    #         elif( r == 14 ):
+    #             # Swap 2 sets of IVs
+    #
+    #         elif( r == 15 ):
+    #             # Increase an IV by an ammount
+    #
+    #         elif( r == 16 ):
+    #             # Decrease an IV by an ammount
+    #
+    #         # Pokemon Item - r&r, None
+    #         # Pokemon Moves - r&r all, r&r one move, None
+    #         # Pokemon Form - change form, evolve, devolve
+    #
+    #         if( randPokeIndex == 0 ):
+    #             child.pokemon1 = toMutate
+    #         if( randPokeIndex == 1 ):
+    #             child.pokemon2 = toMutate
+    #         if( randPokeIndex == 2 ):
+    #             child.pokemon3 = toMutate
+    #         if( randPokeIndex == 3 ):
+    #             child.pokemon4 = toMutate
+    #         if( randPokeIndex == 4 ):
+    #             child.pokemon5 = toMutate
+    #         if( randPokeIndex == 5 ):
+    #             child.pokemon6 = toMutate
+    #     return child
+    #
+    #     # if( random.random() <= mutationRate ):
+    #     #     if( random.randrange(2) == 0 ):
+    #     #         child.x *= random.uniform(-3.0, 3.0)
+    #     #     else:
+    #     #         child.y *= random.uniform(-3.0, 3.0)
+    #     # return child
+    #
     def validation(self, pokemonTeam):
         # TODO: THE REST OF THE GA ISN'T BUILT TO HANDLE NONE MEMBERS
         pokemon = [pokemonTeam.pokemon1, pokemonTeam.pokemon2,
